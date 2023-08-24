8dbf85e764a4f7aac651996d75ac951173861562
bwk73 <bwkersz@gmail.com>
2023-08-24 04:42:25 -0300

    ResultCard redone

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,28 +1,33 @@
 import './styles.css';
 import profileImg from "../../assets/profile_img.png";
+import { InformationDTO } from '../../models/data';
 
-export default function ResultCard() {
+type Props = {
+    information: InformationDTO;
+}
+
+export default function ResultCard({information}: Props) {
 
     return (
 <div className="rc-container">
     <div>
-        <img src={profileImg} alt="profileImg" />
+        <img src={information.profileImg} alt={information.name} />
     </div>
     <div className='rc-information-content'>
         <div>
             <h4>Infomações</h4>
         </div>
         <div className='rc-content-box'>
-            <p>Perfil: https://api.github.com/users/acenelio</p>
+            <span className='fnt-bold'>Perfil:</span>&nbsp;<span className='fnt-blue'>{information.githubUrl}</span>
         </div>
         <div className='rc-content-box'>
-            <p>Seguidores: 4379</p>
+            <span className='fnt-bold'>Seguidores:</span>&nbsp;{information.followers}
         </div>
         <div className='rc-content-box'>
-            <p>Localidade: Uberlândia</p>
+        <span className='fnt-bold'>Localidade:</span>&nbsp;{information.location}
         </div>
         <div className='rc-content-box'>
-            <p>Nome: Nelio Alves</p>
+        <span className='fnt-bold'>Nome:</span>&nbsp;{information.name}
         </div>
     </div>
 </div>
